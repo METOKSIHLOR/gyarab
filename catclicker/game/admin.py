9a10dfb89b1f7039603387a2e3dfbaf8eb49d9b6
METOKS <pressfschool@gmail.com>
2026-05-11 21:05:06 +0200

    added search by model fields in admin panel

--- catclicker/game/admin.py
+++ catclicker/game/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from game.models import Score
 
-admin.site.register(Score)
-
+@admin.register(Score)
+class ScoreAdmin(admin.ModelAdmin):
+    search_fields = ['user__name']
 # Register your models here.
--- catclicker/users/admin.py
+++ catclicker/users/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 
 from users.models import User, UserUpgrade
 
-admin.site.register(User)
-admin.site.register(UserUpgrade)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+@admin.register(UserUpgrade)
+class UpdateAdmin(admin.ModelAdmin):
+    search_fields = ['user__name', "upgrade_name"]
 # Register your models here.
