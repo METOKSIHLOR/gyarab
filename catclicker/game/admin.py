645d4025effe4b75c27b06c73b7d056a69a0b7ca
METOKS <pressfschool@gmail.com>
2026-05-11 21:19:12 +0200

    upgrade admin

--- catclicker/game/admin.py
+++ catclicker/game/admin.py
@@ -5,4 +5,6 @@ from game.models import Score
 @admin.register(Score)
 class ScoreAdmin(admin.ModelAdmin):
     search_fields = ['user__name']
+    list_filter = ['user']
+    list_per_page = 20
 # Register your models here.
--- catclicker/users/admin.py
+++ catclicker/users/admin.py
@@ -3,11 +3,26 @@ from django.contrib import admin
 
 from users.models import User, UserUpgrade
 
+from game.models import Score
+
+
+class ScoreInline(admin.TabularInline):
+    model = Score
+
+class UserUpgradeInline(admin.TabularInline):
+    model = UserUpgrade
+    extra = 1
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     search_fields = ['name']
+    list_per_page = 20
+    list_filter = ['name']
+    inlines = [ScoreInline, UserUpgradeInline]
 
 @admin.register(UserUpgrade)
 class UpdateAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    list_filter = ['user__name', 'upgrade_name']
     search_fields = ['user__name', "upgrade_name"]
 # Register your models here.
